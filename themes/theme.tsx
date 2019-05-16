d851b0c7bf7b387cc9968a8952bcbfeb9703ed5d
Alex Patterson <admin@ajonp.com>
2019-05-16 15:06:31 -0400

    change theme location

--- themes/theme.tsx
+++ themes/theme.tsx
@@ -1,20 +1,16 @@
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import pink from '@material-ui/core/colors/pink';
 import red from '@material-ui/core/colors/red';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      main: '#556cd6'
-    },
-    secondary: {
-      main: '#19857b'
-    },
+    type: 'dark',
+    primary: deepPurple,
+    secondary: pink,
     error: {
       main: red.A400
-    },
-    background: {
-      default: '#fff'
     }
   }
 });
